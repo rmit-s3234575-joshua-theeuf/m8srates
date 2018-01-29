4d0224c8101dc5651abef1e6c1dcaf15e4cb203d
deepozzzie <s3234575@student.rmit.edu.au>
2018-01-30 00:31:48 +1100

    add interest, and change wording in _duedate.haml

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -144,6 +144,7 @@ class Listing < ApplicationRecord
   validates_inclusion_of :valid_until, :allow_nil => :true, :in => proc{ DateTime.now..DateTime.now + 7.months }
   validates_numericality_of :price_cents, :only_integer => true, :greater_than_or_equal_to => 0, :message => "price must be numeric", :allow_nil => true
   validates_inclusion_of :duedate, :allow_nil => :true, :in => proc{ DateTime.now..DateTime.now + 12.months }
+  validates_numericality_of :interest, :only_integer => true, :greater_than_or_equal_to => 0, :message => "Interest must be a numeric value between 0 and 100.", :allow_nil => true
   def self.currently_open(status="open")
     status = "open" if status.blank?
     case status
