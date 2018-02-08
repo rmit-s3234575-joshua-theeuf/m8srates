d3cd5927996635d6df2966caa74b8bc7e4cb36b4
deepozzzie <s3234575@student.rmit.edu.au>
2018-02-08 18:09:45 +1100

    removed repayed

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -148,7 +148,6 @@ class Listing < ApplicationRecord
   validates_inclusion_of :duedate, :allow_nil => :true, :in => proc{ DateTime.now..DateTime.now + 12.months }
   validates_numericality_of :interest, :only_integer => true, :greater_than_or_equal_to => 0, :message => "Interest must be a numeric value between 0 and 100.", :allow_nil => true
   validates_numericality_of :number_of_loans, :only_integer => true, :greater_than_or_equal_to => 1, :message => "Must have at least 1 loan.", :allow_nil => true
-  validates_numericality_of :loan_repayed, :only_integer=>true, :equal_to=>1,:allow_nil=>true
 
   def self.currently_open(status="open")
     status = "open" if status.blank?
